7229b769bca20f86af584db9d4fc2fe836cb1594
easonLiang <liangyx25@163.com>
2023-06-07 09:09:06 +0800

    <autoInferDNN> in2_out2_h1n2.cpp

---
+++ autoInferDNN/in2_out2_h1n2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+#define input_i1 0.05
+#define input_i2 0.1
+#define output_o1 0.13
+#define output_o2 0.99
+
+#define EXCLUDE_MAGIC_FLOAT 98765.01234f
+
+
+int main()
+{
+	cout<<"DNN for a 3 layer neural network"<<endl;
+	cout<<"Layer-start-input input_i1 : "<<input_i1<<" ; input_i2 : "<<input_i2<<endl;
+	cout<<"Layer-end-output \"Wanted\" output_o1 : "<<output_o1<<" ; output_o2 : "<<output_o2<<endl;
+	return 0;
+}
